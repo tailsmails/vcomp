85f37dad3e050a53cfde30b25f03cd3be90e1cb0
tailsmails <253711812+tailsmails@users.noreply.github.com>
2026-07-15 20:22:56 +0430

    Change filter application message and update write call

--- example/example.v
+++ example/example.v
@@ -1,29 +1,25 @@
 import vcomp
 
 fn main() {
-	println('Applying conditional filter...')
+	println('Applying BPF filter...')
 
 	vcomp.new_filter()
 		.set_type(.allowlist)
 		.allow('write').where_arg(0, .eq, 1)
 		.allow('exit_group')
+		.allow('exit')
+		.allow('close')
+		.allow('munmap')
+		.allow('mprotect')
 		.apply() or {
 			println('Failed to apply filter: ${err}')
 			return
 		}
 
-	println('Filter applied successfully!')
 	println('Testing write to stdout (FD 1)...')
 
-	vcomp.call('write', 1, 'This is allowed!\n', 17) or {
+	vcomp.call('write', 1, 'This is allowed and will exit cleanly!\n', 38) or {
 		println(err)
-		return
-	}
-
-	println('Testing write to stderr (FD 2)...')
-
-	vcomp.call('write', 2, 'This will be blocked!\n', 22) or {
-		println('Error: ${err}')
 		return
 	}
 }
