6ce889201c1954285cc1742acba0be7c5b1fa0b1
tailsmails <253711812+tailsmails@users.noreply.github.com>
2026-07-15 19:05:13 +0430

    more clean

--- example/example.v
+++ example/example.v
@@ -1,7 +1,4 @@
 import vcomp
-import os
-
-fn C.syscall(number int, arg1 voidptr, arg2 voidptr, arg3 voidptr, arg4 voidptr) int
 
 fn main() {
 	println('Applying BPF filter...')
@@ -14,16 +11,9 @@ fn main() {
 	println('Filter applied successfully!')
 	println('Testing ptrace block...')
 
-	ptrace_num := vcomp.get_syscall_number('ptrace') or {
-		println(err)
+	res := vcomp.call('ptrace', 0, 0, 0, 0) or {
+		println('ptrace failed as expected! Error details: ${err}')
 		return
 	}
-
-	res := unsafe { C.syscall(ptrace_num, voidptr(0), voidptr(0), voidptr(0), voidptr(0)) }
-	if res == -1 {
-		err_msg := os.error_posix()
-		println('ptrace failed as expected! Error details: ${err_msg}')
-	} else {
-		println('WARNING: ptrace call was allowed.')
-	}
+	println('WARNING: ptrace call was allowed. Result: ${res}')
 }
